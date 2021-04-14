71b74c4149cfca7a50fe68a91056d06bce62af4d
MrGio7 <lashagiorgi1233@gmail.com>
2021-04-14 17:20:33 +0400

    working on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,10 @@
+import jsLogo from "../assets/images/jsLogo.png";
+import cssLogo from "../assets/images/cssLogo.png";
+
 function Home() {
     return <div className="home">
+        <img src={jsLogo} className="jsLogo" alt="javascript" />
+        <img src={cssLogo} className="cssLogo" alt="css" />
     </div>
 }
 
