8c73473d9af8f538f50e73f3fce0ba65d032e0e1
MrGio7 <lashagiorgi1233@gmail.com>
2021-06-01 12:22:51 +0400

    finished dropdown css template

--- src/components/templates/dropdown.js
+++ src/components/templates/dropdown.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import Prism from "prismjs";
+
 function Dropdown() {
+    useEffect(() => {
+        Prism.highlightAll()
+    }, []);
+
     return <div className="dropdownTemplate">
         <h1>Dropdown</h1>
         <p>Toggle contextual overlays for displaying lists of links.</p>
@@ -12,6 +19,54 @@ function Dropdown() {
                 <li>Item 3</li>
             </ul>
         </div>
+        <pre>
+        <code className="language-html">
+            {`<div className="dropdownExample">
+    <button type="button">Dropdown</button>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+</div>`}
+        </code>
+    </pre>
+    <pre>
+      <code className="language-css">
+          {`.dropdownExample{
+    display: flex;
+    position:relative;
+    align-items: center;
+    justify-content: center;
+
+    button{
+        color:white;
+        background-color: $purple3;
+        width: 8rem;
+
+        &:hover{
+            + ul {
+                display: block;
+                position: absolute;
+                top:1.3rem;
+                background-color: black;
+            }
+        }
+    }
+
+    ul{
+        display: none;
+        margin: 0px;
+        z-index: 1000;
+        width: 8rem;
+
+        li{
+            margin: .5rem;
+        }
+    }
+}`}
+      </code>
+  </pre>
     </div>
 };
 
